8ff019d78a2e3824bda756509dc00e6ffe6e6c81
Ayush Khandelwal <ayushak255@gmail.com>
2025-10-19 09:46:26 -0400

    Added frontend packages

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad05c1ad23c5c294"; }
+/******/ 		__webpack_require__.h = function() { return "6f7b6cd6ea4a5891"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
