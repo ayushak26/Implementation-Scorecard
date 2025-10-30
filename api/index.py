74f35fa3c884d19f2af1b2d8481d5229ba7b6ead
Ayush Khandelwal <ayushak255@gmail.com>
2025-10-30 10:54:24 -0400

    Use BaseHTTPRequestHandler format

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': '{"status":"healthy"}'
-    }
+# api/index.py
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(b'{"status":"healthy","message":"Works!"}')
+        return
